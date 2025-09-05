bf86e381b50d9a887a882d28433667b590d8d2c3
Deezi <deezitheviper@gmail.com>
2025-09-05 23:54:33 +0100

    assets

---
+++ assets/icons/index.ts
@@ -0,0 +1,6 @@
+import home from './home.png';
+import profile from './profile.png';
+import save from './save.png';
+import search from './search.png';
+
+export const icons = {home, profile, save, search}
